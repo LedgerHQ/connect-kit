df55d7e8cb5bda970ec5f9fa6bb0a6996548dc4a
Hugo Lopes <hugo.lopes-ext@ledger.fr>
2022-08-23 15:30:47 +0100

    Improve ExtensionUnavailableModal component
    - update SVG icons
    - improve UI styling
    - simplify JSX structure and move close button to the Modal component
    - extend button component and pass the variant prop

--- packages/connect-kit/src/components/ExtensionUnavailableModal/ExtensionUnavailableModal.tsx
+++ packages/connect-kit/src/components/ExtensionUnavailableModal/ExtensionUnavailableModal.tsx
@@ -1,33 +1,17 @@
-import Modal, { setIsModalOpen } from "../Modal";
+import Modal from "../Modal/Modal";
 import {
-  CloseButtonWrapper,
-} from "../Modal/index.styles";
+  ModalSection,
+  ModalTitle,
+} from "../Modal/Modal.styles";
 import {
-  CardBody,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardWrapper,
-  CardSubtitle,
-  CardTitle,
-} from "../Modal/Card.styles";
-import {
-  IconContainer,
-  JoinBetaButton,
-  JoinBetaText,
-  ModalSubtitleContainer,
+  InstallButton,
+  ConnectFeature,
 } from "./ExtensionUnavailableModal.styles";
-import NeedALedgerCard from "../NeedALedgerCard";
-import { default as LedgerConnectSvg } from "../../assets/svg/LedgerConnect.svg";
-import { default as XButtonSvg } from "../../assets/svg/X.svg";
 import { default as LightbulbSvg } from "../../assets/svg/Lightbulb.svg";
 import { default as CheckmarkSvg } from "../../assets/svg/Checkmark.svg";
+import NeedALedgerSection from "../NeedALedgerSection";
 
 const ExtensionUnavailableModal = () => {
-  const handleClose = () => {
-    setIsModalOpen(false);
-  };
-
   const onJoinBetaClick = () => {
     window.open("https://get-connect.ledger.com/", "_blank");
   };
@@ -35,41 +19,20 @@ const ExtensionUnavailableModal = () => {
   return (
     <Modal>
       <>
-        <CardWrapper>
-          <CardContent>
-            <CardHeader>
-              <img src={LedgerConnectSvg} />
-              <CloseButtonWrapper onClick={handleClose}>
-                <img src={XButtonSvg} />
-              </CloseButtonWrapper>
-            </CardHeader>
-            <CardBody>
-              <CardTitle> Try Ledger Connect </CardTitle>
-              <ModalSubtitleContainer>
-                <IconContainer>
-                  <img src={LightbulbSvg} />
-                </IconContainer>
-                <CardSubtitle>
-                  One secure wallet and extension that works across all dApps.
-                </CardSubtitle>
-              </ModalSubtitleContainer>
-              <ModalSubtitleContainer>
-                <IconContainer>
-                  <img src={CheckmarkSvg} />
-                </IconContainer>
-                <CardSubtitle>
-                  Stay safe and prevent harmful transactions with Web3 Check.
-                </CardSubtitle>
-              </ModalSubtitleContainer>
-            </CardBody>
-            <CardFooter>
-              <JoinBetaButton onClick={onJoinBetaClick}>
-                <JoinBetaText>Join the Beta</JoinBetaText>
-              </JoinBetaButton>
-            </CardFooter>
-          </CardContent>
-        </CardWrapper>
-        <NeedALedgerCard />
+        <ModalSection>
+          <ModalTitle>Try Ledger Connect</ModalTitle>
+          <ConnectFeature>
+            <img src={LightbulbSvg} />
+            One secure wallet and extension that works across all dApps.
+          </ConnectFeature>
+          <ConnectFeature>
+            <img src={CheckmarkSvg} />
+            Stay safe and prevent harmful transactions with Web3 Check.
+          </ConnectFeature>
+          <InstallButton variant="primary" onClick={onJoinBetaClick}>Join the Beta</InstallButton>
+        </ModalSection>
+
+        <NeedALedgerSection />
       </>
     </Modal>
   );
