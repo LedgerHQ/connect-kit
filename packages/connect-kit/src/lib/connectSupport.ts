81522c99811f381eca4eeedd89030ad0ec645cff
Hugo Lopes <hugo.lopes-ext@ledger.fr>
2022-12-22 15:25:47 +0000

    Enable Connect support on macOS

--- packages/connect-kit/src/lib/connectSupport.ts
+++ packages/connect-kit/src/lib/connectSupport.ts
@@ -3,7 +3,7 @@ import { Device } from "./browser";
 // check if Connect supports the user's platform
 export function isLedgerConnectSupported (device: Device): boolean {
   return (
-    device.os.name === "iOS" &&
+    (device.os.name === "iOS" || device.os.name === "macOS") &&
     device.browser.name === "Safari"
   );
 }
