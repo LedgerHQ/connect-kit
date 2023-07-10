8d0b95f979faf13d32840f0208b5b715e10ccb33
RamyEB <ramyelb@gmail.com>
2023-07-10 14:20:33 +0200

    fix: init segment + analytics

--- packages/connect-kit-loader/src/index.ts
+++ packages/connect-kit-loader/src/index.ts
@@ -88,27 +88,100 @@ export interface LedgerConnectKit {
   getProvider: GetProviderFunction;
 }
 
-// load Analytics
+const initAnalytics = () => {
+  var analytics = (window.analytics = window.analytics || []);
 
-function getAnalytics() {
-  const analytics = Analytics({
-    app: "connect-kit",
-    plugins: [
-      segmentPlugin({
-        writeKey: "KEY HERE",
-      }),
-    ],
-  });
-  if (typeof analytics === "undefined") {
-    console.log(new Error("analytics must not be undefined!"));
+  // Create a queue, but don't obliterate an existing one!
+  // If the real analytics.js is already on the page return.
+  if (analytics.initialize) return analytics;
+  console.log("init analytics 1");
+  // If the snippet was invoked already show an error.
+  if (analytics.invoked) {
+    if (window.console && console.error) {
+      console.error("Segment snippet included twice.");
+    }
+    return;
   }
+  console.log("init analytics 2");
+
+  // Invoked flag, to make sure the snippet
+  // is never invoked twice.
+  analytics.invoked = true;
+  // A list of the methods in Analytics.js to stub.
+  analytics.methods = [
+    "trackSubmit",
+    "trackClick",
+    "trackLink",
+    "trackForm",
+    "pageview",
+    "identify",
+    "reset",
+    "group",
+    "track",
+    "ready",
+    "alias",
+    "debug",
+    "page",
+    "once",
+    "off",
+    "on",
+    "addSourceMiddleware",
+    "addIntegrationMiddleware",
+    "setAnonymousId",
+    "addDestinationMiddleware",
+  ];
+  // Define a factory to create stubs. These are placeholders
+  // for methods in Analytics.js so that you never have to wait
+  // for it to load to actually record data. The `method` is
+  // stored as the first argument, so we can replay the data.
+  analytics.factory = function (method: any) {
+    return function () {
+      var args = Array.prototype.slice.call(arguments);
+      args.unshift(method);
+      analytics.push(args);
+      return analytics;
+    };
+  };
+  // For each of our methods, generate a queueing stub.
+  for (var i = 0; i < analytics.methods.length; i++) {
+    var key = analytics.methods[i];
+    analytics[key] = analytics.factory(key);
+  }
+  // Define a method to load Analytics.js from our CDN,
+  // and that will be sure to only ever load it once.
+  analytics.load = function (key: any, options: any) {
+    // Create an async script element based on your key.
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.async = true;
+    script.src =
+      "https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";
+    // Insert our script next to the first script element.
+    var first = document.getElementsByTagName("script")[0];
+    first.parentNode?.insertBefore(script, first);
+    analytics._loadOptions = options;
+  };
+  analytics._writeKey = "private_key";
+  // Add a version to keep track of what's in the wild.
+  analytics.SNIPPET_VERSION = "4.15.2";
+  // Load Analytics.js with your key, which will automatically
+  // load the tools you've enabled for your account. Boosh!
+  analytics.load("private_key");
+  // Make the first page call to load the integrations. If
+  // you'd like to manually name or tag the page, edit or
+  // move this call however you'd like.
+
   return analytics;
-}
+};
 
 export async function loadConnectKit(): Promise<LedgerConnectKit> {
-  const src = "https://cdn.jsdelivr.net/npm/@ledgerhq/connect-kit@1";
+  const src = "connect-kit.vercel.app/umd/index.js";
   const globalName = "ledgerConnectKit";
-  const analytics = getAnalytics();
+  const analytics = initAnalytics();
+
+  analytics.track("New Connection", {
+    host: window.location.hostname,
+  });
 
   return new Promise((resolve, reject) => {
     const scriptId = `ledger-ck-script-${globalName}`;
