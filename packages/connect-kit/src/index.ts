3a0e3ac55c931bb5a27d778f5046ed2af7c43f3a
Samy RABAH-MONTAROU <samy.rabah-montarou@ledger.fr>
2024-01-15 13:05:12 +0100

    export ethereumprovider and supportedproviders from connect kit directly

--- packages/connect-kit/src/index.ts
+++ packages/connect-kit/src/index.ts
@@ -1,3 +1,4 @@
 export { checkSupport } from "./lib/support";
-export { getProvider } from "./lib/provider";
+export { getProvider, SupportedProviders } from "./lib/provider";
 export { enableDebugLogs } from './lib/logger';
+export { EthereumProvider } from './providers/ExtensionEvm';
